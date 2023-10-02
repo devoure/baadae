3bb6a3f9c7cc404ce482337d701c350ce5e24d83
Athumani Bakari <disguisedsandwich@gmail.com>
2023-10-02 21:51:02 +0300

    FRONTEND: Added more sections to login page

--- frontend/baadae-frontend/src/components/SignIn.jsx
+++ frontend/baadae-frontend/src/components/SignIn.jsx
@@ -5,9 +5,9 @@ import {  AiFillFacebook } from "react-icons/ai"
 
 function SignIn(){
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div className="w-full h-full flex justify-center items-center relative">
       
-      <div className="w-[90%] h-[90%]">
+      <div className="w-[90%] h-[90%]  flex flex-col justify-between">
 
         <div className="p-0 m-0 flex justify-start">
           <img src = { siteLogo }  className="max-w-[4rem] max-h-[4rem]"/>
@@ -21,38 +21,50 @@ function SignIn(){
           <span className="tracking-normal leading-relaxed font-roboto text-3xl font-bold text-[#220e0a] whitespace-nowrap">Share here.</span>
         </div>
 
-        <div className="flex-col min-w-80 h-full">
+        <div className="flex-col w-96 font-roboto">
 
-          <div className="flex items-center justify-center border-2 border-[#baaf98] rounded-3xl text-base font-roboto font-semibold text-[#dd4f43] p-1 mb-2">
+          <div className="flex items-center justify-center border border-[#baaf98] rounded-3xl text-base font-roboto font-semibold text-[#dd4f43] p-1 mb-2 cursor-pointer">
             <  AiFillGoogleSquare  className="text-4xl text-[#dd4f43]"/>
             <span className="ml-2 whitespace-nowrap">Sign up with Google</span>
           </div>
 
-          <div className="flex items-center justify-center border-2 border-[#baaf98] rounded-3xl text-base font-roboto font-semibold text-[#1a4789] p-1 mb-2">
+          <div className="flex items-center justify-center border border-[#baaf98] rounded-3xl text-base font-roboto font-semibold text-[#1a4789] p-1 mb-2 cursor-pointer">
             < AiFillFacebook className="text-4xl text-[#1a4789]"/>
             <span className="ml-2 whitespace-nowrap">Sign up with Facebook</span>
           </div>
 
-          <div className="flex items-center justify-center border-2 border-[#baaf98] rounded-3xl text-base font-roboto font-semibold text-[#1b9df0] p-1 mb-2">
+          <div className="flex items-center justify-center border border-[#baaf98] rounded-3xl text-base font-roboto font-semibold text-[#1b9df0] p-1 mb-2 cursor-pointer">
             < AiFillTwitterCircle className="text-4xl text-[#1b9df0]"/>
             <span className="ml-2 whitespace-nowrap">Sign up with Twitter</span>
           </div>
 
         </div>
 
-        <div>
+        <div className="flex py-2 w-96 items-center justify-start">
+          <hr className="border border-[#baaf98] w-44"/><span className="font-roboto font-semibold px-4 text-[#220e0a]">or</span><hr className="border border-[#baaf98] w-40"/>
         </div>
 
-        <div>
+        <div  className="flex items-center justify-center w-96  border-0 bg-[#220e0a] rounded-3xl text-base font-roboto font-semibold text-[#d6a97d] p-3 cursor-pointer">
+            <span className="ml-2 whitespace-nowrap">Create account</span>
         </div>
 
-        <div>
+        <div className="w-96 p-2 text-xs font-roboto text-[#baaf98] font-semibold">
+          <span>By signing up, you agree to the <span className="text-[#220e0a]">Terms of Service</span> and been a <span className="text-[#220e0a]">Manchester United</span> fan</span>
         </div>
 
-        <div>
+        <div className="font-roboto text-[#220e0a] font-semibold p-2 mt-4 text-lg">
+          <span className="whitespace-nowrap">Already have an account ?</span>
         </div>
 
-        <div>
+        <div  className="flex items-center justify-center w-96  border border-[#baaf98] rounded-3xl text-base font-roboto font-semibold text-[#220e0a] p-3 cursor-pointer">
+            <span className="ml-2 whitespace-nowrap">Sign in</span>
+        </div>
+
+      </div>
+
+      <div className="absolute inset-0 bg-black hidden">
+
+        <div className="">
         </div>
 
       </div>
