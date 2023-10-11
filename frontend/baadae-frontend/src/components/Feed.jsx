fb1c7e1a0362d7a3201e8ca71934b9674e2187cb
Athumani Bakari <disguisedsandwich@gmail.com>
2023-10-11 21:47:28 +0300

    FRONTEND: Fixed people and feed components

--- frontend/baadae-frontend/src/components/Feed.jsx
+++ frontend/baadae-frontend/src/components/Feed.jsx
@@ -7,20 +7,20 @@ function Feed(){
 
   const feedsCard = feeds.map((feed)=>{
     return(
-      <div className="group flex flex-col justify-between w-full h-[320px] mb-5 hover:bg-[#fdf9f8] cursor-pointer" key={ feed.id }>
+      <div className="group flex flex-col justify-between items-end w-full min-h-[420px] mb-10 hover:bg-[#fafafa] cursor-pointer pt-5 tablet:min-h-[430px]" key={ feed.id }>
         <div className="flex h-12 w-full font-roboto font-semibold text-sm items-center justify-between select-none text-[#baaf98] px-5">
           <span className="flex items-center"><AiFillHeart className="text-2xl inline mr-2"/>Bruno Liked</span>
           <span>~ { feed.time } ago</span>
         </div>
 
-        <div className="flex items-centerh-16 w-full px-5 select-none">
+        <div className="flex items-center h-16 w-full px-5 select-none">
           <img src={ feed.profpic } className="w-14 h-14 object-cover rounded-full"/>
           <div className="flex flex-col h-full font-roboto font-semibold ml-5">
-            <span className="text-[#220e0c]">{ feed.name }</span>
-            <span className="text-[#4f352a] text-sm pt-2">{ feed.desc }</span>
+            <span className="text-[#220e0c] tablet:text-lg">{ feed.name }</span>
+            <span className="text-[#4f352a] text-sm tablet:text-lg pt-2">{ feed.desc }</span>
           </div>
         </div>
-        <div className="flex w-full h-[220px] bg-white relative">
+        <div className="flex w-[80%] h-[340px] bg-white mr-5 relative rounded-xl overflow-hidden">
           <img src={ feed.bookmark } className="w-full h-full object-cover"/>
           <div className="absolute flex items-center justify-center inset-x-0 bottom-0 h-0 group-hover:h-16 bg-[#220e0acc] overflow-hidden transition-all duration-300">
             <div className="h-full w-16 mr-5 flex flex-col items-center justify-center font-roboto font-semibold">
@@ -37,7 +37,7 @@ function Feed(){
     )
   })
   return (
-    <div className="flex flex-col w-[375px] pt-[150px]">
+    <div className="flex flex-col min-w-[375px] pt-[130px] tablet:max-w-[600px]">
       { feedsCard }
     </div>
   )
--- frontend/baadae-frontend/src/components/People.jsx
+++ frontend/baadae-frontend/src/components/People.jsx
@@ -7,20 +7,20 @@ function People(){
   const peopleCard = people.map((person)=>{
     return(
       <div className="flex h-32 tablet:h-40 w-full items-center pl-5 select-none cursor-pointer mb-5 hover:bg-[#ebebeb] transition duration-300" key={ person.id }>
-        <img src={ person.profpic } className="w-20 h-20 tablet:w-32 tablet:h-32 rounded-full object-cover"/>
+        <img src={ person.profpic } className="w-20 h-20 rounded-full object-cover"/>
         <div className="flex  flex-col pl-10 font-roboto">
-          <span className="font-semibold font-[#220e0a] text-xl tablet:text-2xl pb-2">{ person.name }</span>
+          <span className="font-semibold font-[#220e0a] text-xl pb-2">{ person.name }</span>
           <div className="flex items-center">
-            < MdEmail className="text-xl tablet:text-2xl mr-3 text-[#d6a97d]"/>
-            <span className="text-sm tablet:text-lg text-[#220e0a]">{ person.email }</span>
+            < MdEmail className="text-xl mr-3 text-[#d6a97d]"/>
+            <span className="text-sm text-[#220e0a]">{ person.email }</span>
           </div>
           <div className="flex items-center">
-            <AiFillHeart className="text-xl tablet:text-2xl mr-3 text-[#d6a97d]"/>
-            <span className="text-sm tablet:text-lg text-[#220e0a]">{ person.likes } likes </span>
+            <AiFillHeart className="text-xl mr-3 text-[#d6a97d]"/>
+            <span className="text-sm text-[#220e0a]">{ person.likes } likes </span>
           </div>
           <div className="flex items-center">
             <BsBookmarkFill className="text-lg tablet:text-2xl mr-3 text-[#d6a97d]"/>
-            <span className="text-sm text-[#220e0a] tablet:text-lg ">{ person.bookmarks } bookmarks</span>
+            <span className="text-sm text-[#220e0a]">{ person.bookmarks } bookmarks</span>
           </div>
         </div>
       </div>
