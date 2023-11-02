4b9a6352e8b03e4b5e5bdd4916aa5a4540e28e4e
Athumani Bakari <disguisedsandwich@gmail.com>
2023-11-02 21:07:10 +0300

    BACKEND:Added Sign up comp

--- backend/baadae/accounts/api/serializers.py
+++ backend/baadae/accounts/api/serializers.py
@@ -3,6 +3,24 @@ from django.contrib.auth.models import User
 
 
 class UserSerializers(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={"input_type": "password"},
+                                      write_only=True)
+
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'email', 'password')
+        fields = ('username', 'first_name', 'email', 'password2')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def save(self):
+        user = User(email=self.validate_data['email'],
+                    username=self.validated_data['username'],
+                    first_name=self.validated_data['first_name'])
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        if password != password2:
+            raise serializers.ValidationError({
+                'password': 'Password must watch.'
+                })
+        user.set_password(password)
+        user.save()
+        return user
--- backend/baadae/accounts/api/views.py
+++ backend/baadae/accounts/api/views.py
@@ -1,8 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import UserSerializers
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -20,3 +23,11 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
+
+@api_view((['POST']))
+def add_user(request):
+    new_user = UserSerializers(data=request.data)
+    if new_user.is_valid():
+        new_user.save()
+        return Response("OK")
+    return Response("ERROR")
