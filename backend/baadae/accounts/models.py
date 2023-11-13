4362a6d083953141b12d57815e81f17a0ba61bad
Athumani Bakari <disguisedsandwich@gmail.com>
2023-11-13 21:59:44 +0300

    BACKEND: Added profile model

--- backend/baadae/accounts/models.py
+++ backend/baadae/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+    location = models.CharField(max_length=50)
+    bio = models.CharField(max_length=200)
+    banner = models.ImageField(upload_to='users/banner/', blank=True)
+
+    def __str__(self):
+        return f'Profile for { self.user.username }'
--- backend/baadae/baadae/settings.py
+++ backend/baadae/baadae/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -166,3 +167,7 @@ SIMPLE_JWT = {
 
 # Cors
 CORS_ALLOW_ALL_ORIGINS = True
+
+# Images
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
