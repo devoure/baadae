e32ca3b087c952183e02c4118a2aa0bb446c82a8
Athumani Bakari <disguisedsandwich@gmail.com>
2023-12-10 23:52:18 +0300

    FRONTEND: Added responsive on add bookamrk

--- frontend/baadae-frontend/src/components/AddBookmark.jsx
+++ frontend/baadae-frontend/src/components/AddBookmark.jsx
@@ -33,8 +33,8 @@ function AddBookmark(props){
   }
 
   return (
-    <div className="inset-0  bg-[#220e0acc] absolute z-50 flex items-center justify-center">
-      <div className="w-[350px] h-[600px] bg-white rounded-[2rem] shadow-sm overflow-hidden">
+    <div className="inset-x-0 h-screen bg-[#220e0acc] fixed z-50 flex items-center justify-center">
+      <div className="w-[350px] h-[600px] tablet:w-[450px] bg-white rounded-[2rem] shadow-sm overflow-hidden">
         <div className="w-full h-[4rem] flex items-center">
           <div className="w-[32px] h-[32px] bg-white rounded-full ml-4 hover:bg-[#d6dee2] cursor-pointer flex items-center justify-center transition duration-300" onClick={ props.addBookmarkComp }>
             <MdClose className="text-2xl text-[#220e0a]"/>
